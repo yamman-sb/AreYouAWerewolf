2d93a2099decf54572f938277ca7218d9c1c66d9
Yosuke Yamashita <yamman.sb@gmail.com>
2021-05-20 10:45:11 +0900

    [add]イモ娘のステータスが見られるように変更

--- src/Imo.java
+++ src/Imo.java
@@ -1,26 +1,35 @@
+import java.util.List;
+
 public class Imo {
+
+  // イモ娘の名前
+  private final String name;
+
+  // ステータス
   private final Status sweetness;
   private final Status nutrition;
   private final Status smell;
   private final Status resistance;
   private final Status productivity;
 
-  public Imo(int sweetnessScore,    int sweetnessGrowthRate,
+
+  public Imo(String name,
+             int sweetnessScore,    int sweetnessGrowthRate,
              int nutritionScore,    int nutritionGrowthRate,
              int smellScore,        int smellGrowthRate,
              int resistanceScore,   int resistanceGrowthRate,
              int productivityScore, int productivityGrowthRate) {
+    this.name         = name;
     this.sweetness    = new Status("甘さ",  sweetnessScore,     sweetnessGrowthRate);
     this.nutrition    = new Status("栄養",  nutritionScore,     nutritionGrowthRate);
     this.smell        = new Status("匂い",  smellScore,         smellGrowthRate);
     this.resistance   = new Status("耐性",  resistanceScore,    resistanceGrowthRate);
     this.productivity = new Status("生産性", productivityScore,  productivityGrowthRate);
-
-
   }
 
-  public Status getSweetness() {
-    return this.sweetness;
+  public List<String> getStatusToString() {
+    return List.of(name, sweetness.toString(), nutrition.toString(),
+        smell.toString(), resistance.toString(), productivity.toString());
   }
 
   public class Status {
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,37 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class Main {
   public static void main(String[] args){
-    Imo imo = new Imo(1,10,
+
+    Consumer<ArrayList<String>> showStatus = new Consumer<ArrayList<String>>() {
+      @Override
+      public void accept(ArrayList arrayList) {
+        for (Object index : arrayList) {
+          System.out.println(index);
+        }
+        System.out.println();
+      }
+    };
+
+    Imo kintoki = new Imo("金時",
+        1,10,
+        1,  10,
+        1,  0,
+        1,  0,
+        1,  10);
+    ArrayList<String> kintokiStatus = new ArrayList<>(kintoki.getStatusToString());
+    showStatus.accept(kintokiStatus);
+
+    Imo beniazuma = new Imo("紅あずま",
+        1,10,
         1,  10,
         1,  0,
         1,  0,
         1,  10);
 
-
-
-    System.out.println(imo.getSweetness().toString());
+    ArrayList<String> beniazumaStatus = new ArrayList<String>(beniazuma.getStatusToString());
+    showStatus.accept(beniazumaStatus);
   }
 }

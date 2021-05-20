ae02d35d943d9f96b8deb555f5b28bb1cf57232c
Yosuke Yamashita <yamman.sb@gmail.com>
2021-05-20 13:48:28 +0900

    [add]イモ娘を選ぶ機能を追加

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
   public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
 
     /**
      * イモ娘のステータスを表示する
@@ -13,22 +20,43 @@ public class Main {
       for (Object imoStatus : imoStatusArray) {
         System.out.println(imoStatus);
       }
-      System.out.println();
+      System.out.println();};
+
+    /**
+     * メタイモ娘の一覧を返す
+     */
+    Runnable showIndexMetaimo = () -> {for (MetaImo metaImo : MetaImo.values()) {
+      System.out.printf("%s:%s\n", metaImo.getId(), metaImo.getName()); }};
+
+    /**
+     * かずささんが話す
+     */
+    Consumer<String> speackKazusa = (text) -> {
+      System.out.printf("かずさ「%s」\n", text);
     };
+
+
+    // 育成するイモ娘を選択する
+    speackKazusa.accept("育成するイモ娘を選択してください");
 
     ArrayList<String> kintokiStatus = new ArrayList<>(MetaImo.KINTOKI.getNameAndStatusToString());
     showStatus.accept(kintokiStatus);
 
     ArrayList<String> beniazumaStatus = new ArrayList<String>(MetaImo.KINTOKI.getNameAndStatusToString());
     showStatus.accept(beniazumaStatus);
+
+    showIndexMetaimo.run();
+    String  selectedId = scanner.nextLine();
+    MetaImo traningMetaImo = MetaImo.searchId(selectedId);
+    speackKazusa.accept(traningMetaImo.getName()+ "が選ばれました");
+
+
+
+    // 因子を引き継ぐイモ娘を選択する
+
+    // イモ娘の育成を行う
+
+
+    // 育成したイモ娘の因子を決定する
   }
-
-  // 育成するイモ娘を選択する
-
-
-  // 因子を引き継ぐイモ娘を選択する
-
-  // イモ娘の育成を行う
-
-  // 育成したイモ娘の因子を決定する
 }
--- src/MetaImo.java
+++ src/MetaImo.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -6,19 +7,20 @@ import java.util.List;
  * csvに保存するという方法も考えたが、結局は表示methodが必要になるので今は保留
  */
 public enum MetaImo {
-  KINTOKI("金時",
+  KINTOKI("1","金時",
       86, 0,
       91, 20,
       83, 0,
       96, 10,
       94, 0),
-  BENIAZUMA("紅あずま",
-              93, 10,
-              89, 20,
-              100, 0,
-              89, 0,
-              79, 0),;
+  BENIAZUMA("2","紅あずま",
+      93,   10,
+      89,   20,
+      100,  0,
+      89,   0,
+      79,   0),;
 
+  private final String id; // 計算しないのでString
   // イモ娘の名前
   private final String name;
 
@@ -29,12 +31,13 @@ public enum MetaImo {
   private final Status resistance;
   private final Status productivity;
 
-  private MetaImo(String name,
+  private MetaImo(String id, String name,
              int sweetnessScore,    int sweetnessGrowthRate,
              int nutritionScore,    int nutritionGrowthRate,
              int smellScore,        int smellGrowthRate,
              int resistanceScore,   int resistanceGrowthRate,
              int productivityScore, int productivityGrowthRate) {
+    this.id           = id;
     this.name         = name;
     this.sweetness    = new Status(StatusName.SWEETNESS.toStoring(),    sweetnessScore,     sweetnessGrowthRate);
     this.nutrition    = new Status(StatusName.NUTRITION.toStoring(),    nutritionScore,     nutritionGrowthRate);
@@ -50,5 +53,38 @@ public enum MetaImo {
   public List<String> getNameAndStatusToString() {
     return List.of(name, sweetness.toString(), nutrition.toString(),
         smell.toString(), resistance.toString(), productivity.toString());
+  }
+
+  /**
+   * イモ娘の名前を返す
+   * @return
+   */
+  public String getName() {
+    return this.name;
+  }
+
+  public String getId() {
+    return this.id;
+  }
+
+  /**
+   * イモ娘の一覧を配列で返す
+   * @return
+   */
+  public static List<String> getIndexNames() {
+    List<String> names = new ArrayList<String>();
+    for (MetaImo imo : MetaImo.values()) {
+      names.add(imo.toString());
+    }
+    return names;
+  }
+
+  public static MetaImo searchId(String searchId) {
+    for (MetaImo metaImo : MetaImo.values()) {
+      if (metaImo.getId().equals(searchId)) {
+        return metaImo;
+      }
+    }
+    return null;
   }
 }
